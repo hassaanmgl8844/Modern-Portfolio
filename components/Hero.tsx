e4ee09c0923057e534597ee7af83ff1141bf8b34
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-23 10:33:56 +0500

    Added some code from next js website for animation words

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -15,6 +16,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
+          <TextGenerateEffect className="" words="" />
         </div>
       </div>
     </div>
