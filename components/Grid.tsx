dc3a46a21336a976ef34f572806427d867809525
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-23 11:55:37 +0500

    After lot Of Efforts We have Rendered a Navbar

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id='about'>
+        <BentoGrid>
+            {[{title: 'Title 1', description: 'Description1', id:1}].map((item) =>(
+                <BentoGridItem id={item.id} key={item.id} title={item.title} description={item.description} />
+            ))}
+        </BentoGrid>
+    </section>
   )
 }
 
