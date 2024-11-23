cf37fc19bfafac341deecf971e0583c3c28a7495
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-23 10:39:13 +0500

    MashaAllah Working in Homepage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,10 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
-          <TextGenerateEffect className="" words="" />
+          <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Concepts into Seamless Experiences" />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi I&apos;m Hassaam , A MERN Stack Developer Based in Pakistan.
+          </p>
         </div>
       </div>
     </div>
