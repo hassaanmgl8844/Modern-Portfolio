a19c51f0cd1c6de7087a90c0ea11250a1548b579
hassaanmgl8844 <hsnprog8844@gmail.com>
2024-11-23 12:08:27 +0500

    Added some Assets for my Project

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 const Grid = () => {
   return (
-    <section id='about'>
-        <BentoGrid>
-            {[{title: 'Title 1', description: 'Description1', id:1}].map((item) =>(
-                <BentoGridItem id={item.id} key={item.id} title={item.title} description={item.description} />
-            ))}
-        </BentoGrid>
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
     </section>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
